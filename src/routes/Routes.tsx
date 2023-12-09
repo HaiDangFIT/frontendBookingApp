ffc034315b442093e73a9b1a7e3a1f5051589b9a
HaiDangFIT <dangtqh.fit@gmail.com>
2023-12-10 04:43:43 +0700

    Add Route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,9 +1,15 @@
+import DoctorDetail from '../pages/doctor/doctor-detail/DoctorDetail';
+import DoctorList from '../pages/doctor/doctor-list/DoctorList';
 import Home from '../pages/home/HomePage/Home';
 import Login from '../pages/login/Login';
+import Register from '../pages/register/Register';
 
 const routes = {
   home: '/',
-  login: '/login'
+  login: '/login',
+  register: '/register',
+  doctorDetail:'/doctor-detail',
+  doctorList:'/doctor-list',
 };
 
 export const privateRoutes = [];
@@ -11,4 +17,7 @@ export const privateRoutes = [];
 export const publicRoutes = [
   { path: routes.home, component: Home, layout: true },
   { path: routes.login, component: Login, layout: false},
+  { path: routes.register, component: Register, layout: false},
+  { path: routes.doctorDetail, component: DoctorDetail, layout: true},
+  { path: routes.doctorList, component: DoctorList, layout: true},
 ];
