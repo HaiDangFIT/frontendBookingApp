98975a5b7b3433d127a1f9a03b037929bd7fcb29
HaiDangFIT <dangtqh.fit@gmail.com>
2023-12-10 04:45:14 +0700

    Another change

--- src/pages/home/HomePage/Home.tsx
+++ src/pages/home/HomePage/Home.tsx
@@ -3,16 +3,22 @@ import Featured from "../Featured/Featured";
 import './style.scss'
 import PopularDoctor from "../PopularDoctor/PopularDoctor";
 import PopularClinic from "../PopularClinic/PopularClinic";
+import Header from "../../../components/layout/header/Header";
 
 const Home = () => {
   return(
-    <div className="homeContainer">
-      <Featured/>
-      <Typography variant="h1" className="homeTitle">Bác sĩ nổi bật</Typography>
-      <PopularDoctor/>
-      <Typography variant="h1" className="homeTitle">Bệnh viện nổi bật</Typography>
-      <PopularClinic/>
-    </div>
+    <>
+      <Header/>
+      <div className="homeContainer">
+        
+        <Featured/>
+        <Typography variant="h1" className="homeTitle">Bác sĩ nổi bật</Typography>
+        <PopularDoctor/>
+        <Typography variant="h1" className="homeTitle">Bệnh viện nổi bật</Typography>
+        <PopularClinic/>
+      </div>
+    </>
+    
   )
 };
 
